b3449669c2236f258d0e6f883f3a9239edc82f11
sokol08 <sokol08.an.nsk@mail.ru>
2025-05-18 17:40:04 +0700

    clean

--- sci-electronics/rars/files/wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Wrap the jar file
-DIR="%%INSTALLDIR%%"
-JAR="%%JAR%%"
-java -Dawt.useSystemAAFontSettings=on \
-     -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel \
-     -jar "${DIR}/${JAR}" "${@}"
